20061ab3e43f8d18e4f93cea1eb0af425af2ebaf
AdAstraAbyssosque67 <jg2024-0823@uce.edu.do>
2026-03-13 17:49:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ExamenPractico
+{
+    internal class Program
+    {
+        public class Producto
+        {
+            public string Nombre { get; set; }
+            public int Codigo { get; set; }
+            public double Precio { get; set; }
+            public int Cantidad { get; set; }
+
+            public Producto(string nombre, int codigo, double precio, int cantidad)
+            {
+                Nombre = nombre;
+                Codigo = codigo;
+                Precio = precio;
+                Cantidad = cantidad;
+            }
+
+            public virtual void MostrarProducto()
+            {
+                Console.WriteLine($"Nombre: {Nombre}\nCodigo: {Codigo}\nPrecio: {Precio}\nCantidad: {Cantidad}");
+            }
+            public virtual void CalcularImpuesto() { }
+        }
+
+        public class ProductoElectrico : Producto
+        {
+            public int GarantiaMeses { get; set; }
+            public ProductoElectrico(string n, int c, double p, int cant, int g) : base(n, c, p, cant)
+            {
+                GarantiaMeses = g;
+            }
+
+            public override void CalcularImpuesto()
+            {
+                Console.WriteLine($"El Impuesto (18%) es: {Precio * 0.18}");
+            }
+        }
+
+        public class ProductoAlimenticio : Producto
+        {
+            public string FechaVencimiento { get; set; }
+
+            public ProductoAlimenticio(string n, int c, double p, int cant, string f) : base(n, c, p, cant)
+            {
+                FechaVencimiento = f;
+            }
+
+            public override void CalcularImpuesto()
+            {
+                Console.WriteLine($"El Impuesto (8%) es: {Precio * 0.08}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ProductoElectrico A = new ProductoElectrico("PC", 101, 25000, 5, 12);
+            ProductoAlimenticio B = new ProductoAlimenticio("Leche", 202, 50, 10, "20/12/2026");
+
+            Console.WriteLine("Producto Electrico");
+            A.MostrarProducto();
+            A.CalcularImpuesto();
+            Console.WriteLine();
+            Console.WriteLine("Producto Alimenticio");
+            B.MostrarProducto();
+            B.CalcularImpuesto();
+        }
+    }
+}
+
+
